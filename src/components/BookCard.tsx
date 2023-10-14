a8a8e5897c2d978cadf9f315cca05c9e6b8b2049
Selim <selimkerimow68@gmail.com>
2023-10-14 18:13:53 +0500

    added badge to BookCard componente and made few little changes

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,7 +1,7 @@
 import '../css/books.css'
 
 // Ant Design
-import {Card, Button, Image} from 'antd'
+import {Card, Button, Image, Badge} from 'antd'
 
 // react-icons
 import { BsCart4 } from 'react-icons/bs'
@@ -18,23 +18,27 @@ const BookCard: React.FC<BookCardProps> = ({ price, title, image }) => {
 
     return (
         <div>
-            <Card
-            hoverable
-            className='bookcard-main'
-            cover={<Image className='bookcard-image' src={ image } />}
-            bodyStyle={{ height: 165, fontSize: '0.7rem', padding: '5px', lineHeight: 1.3}}
-            >
-            <div className='bookcard-body'>
-                <h2> {price} TMT</h2>
-                <Button danger style={{fontSize: '1rem'}}>
-                    <BsCart4 size={17}/>
-                    Cart
-                </Button>
-            </div>
-            <div className='bookcard-title'>
-                <h2> {title} </h2>
-            </div>
-        </Card>            
+            <Badge.Ribbon text='New'>
+
+                <Card
+                    hoverable
+                    className='bookcard-main'
+                    cover={<Image className='bookcard-image' src={ image } />}
+                    bodyStyle={{ height: 165, fontSize: '0.7rem', padding: '5px', lineHeight: 1.3}}
+                    >
+                    <div className='bookcard-body'>
+                        <h2> {price} TMT</h2>
+                        <Button danger style={{fontSize: '1rem'}}>
+                            <BsCart4 size={17}/>
+                            Cart
+                        </Button>
+                    </div>
+                    <div className='bookcard-title'>
+                        <h2> {title} </h2>
+                    </div>
+                </Card>
+
+            </Badge.Ribbon>
         </div>
 
     )
--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,4 +1,5 @@
-import { books_data } from '../project_data/books';
+import { biography } from '../project_data/books';
+import { useLocation } from 'react-router-dom';
 
 // css
 import '../css/books.css'
@@ -6,19 +7,20 @@ import '../css/books.css'
 // components
 import BookCard from './BookCard';
 
+
 const Books: React.FC = () => {
 
-    const booksByCategory = () => {
-        
-    }
+    const location = useLocation()
 
 
     return (
         <div className='book-list'>
             <div className='book-inner'>
-                { books_data.biography?.map(item => 
-                    <BookCard title={item.title} price={item.price} image={item.image}/>
-                    )}                
+
+                { location.pathname == '/books/biography' && biography?.map(item => 
+                    <BookCard title={item.title} price={item.price} image={item.image} key={item.id}/>
+                    )}
+
             </div>
         </div>
     )
