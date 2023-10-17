d27ec8893e0281a1e90ee12fa04ec2dab8bff83b
Selim <selimkerimow68@gmail.com>
2023-10-17 19:01:40 +0500

    modified project data and detail page

--- src/components/BookDetail.tsx
+++ src/components/BookDetail.tsx
@@ -1,6 +1,7 @@
 import { useCookies } from 'react-cookie'
 import '../css/bookDetail.css'
 import { Button, Image } from 'antd'
+import { BsCart4 } from 'react-icons/bs'
 
 
 
@@ -19,18 +20,20 @@ const BookDetail: React.FC = () => {
             <div className='detail-container'>
                 <h2> {cookie.selectedBook.title} </h2>
                 <div className='detail-content'>
-                    <p> <span className='detail-name'>Author:</span> Smbd </p>
-                    <p> <span className='detail-name'>Category:</span> </p>
-                    <p> <span className='detail-name'>Barcode:</span> </p>
-                    <p> <span className='detail-name'>Language:</span> </p>
-                    <p> <span className='detail-name'>Release:</span> </p>
+                    <p> <span className='detail-name'>Author:</span> {cookie.selectedBook.author} </p>
+                    <p> <span className='detail-name'>Category:</span> {cookie.selectedBook.category} </p>
+                    <p> <span className='detail-name'>Barcode:</span> {cookie.selectedBook.barcode}</p>
+                    <p> <span className='detail-name'>Language:</span> {cookie.selectedBook.language}</p>
+                    <p> <span className='detail-name'>Release:</span> {cookie.selectedBook.release}</p>
                 </div>
                 <div className='detail-low'>
                     <p> {cookie.selectedBook.price} TMT</p>
-                    <Button type='primary' danger>Cart</Button>
+                    <Button type='primary' danger className='detail-button'> 
+                        <BsCart4 size={16} style={{ marginRight: 3}}/>Cart
+                    </Button>
                 </div>
                 <div className='detail-description'>
-                «Иметь и не иметь» (1937 г.) – история Гарри Моргана, простого и честного рыбака, который превращается в контрабандиста. В основе повествования – судьба главного героя, ставшего преступником. Но кроме этого, «Иметь и не иметь» – остросоциальный роман, в центре которого – конфликт бедности и богатства, имущих и не имущих, людей, которых бедность вынуждает преступать закон, и людей, наслаждающихся жизнью и прожигающих ее. Кто же из них более достоин порицания?
+                    { cookie.selectedBook.description }
                 </div>
             </div>
 
